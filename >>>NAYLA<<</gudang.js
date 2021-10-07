2cc02d46157f2643f64c9f2eb053d5e8dee344e4
NYX095 <82368552+NYX095@users.noreply.github.com>
2021-10-07 15:30:42 +0700

    Add files via upload

---
+++ >>>NAYLA<<</gudang.js
@@ -0,0 +1,86 @@
+{	
+	"prefix": "!",
+
+		
+	"kelas": "3 SMP",
+	
+	
+	"gender": "CWK",
+	
+	
+	"agama": "ISLAM",
+	
+	
+	"asal": "JATIM",
+	
+	
+	"umur": "19 TAHUN",
+	
+	
+	"contoh": "Kurumi",
+	
+		
+	"donasi": "CIEE MAU DONASI SI AJG🗿",
+	
+	
+	"pulsa": "081216465289",
+	
+	
+	"gopay": "085815163593",
+	
+	
+	"ovo": "081216465289",
+	
+	
+	"dana": "085815163593",
+	
+	
+	"namabot": "ZBOTZ",
+	
+	
+	"NamaOwner": "NYX KWONTOL",
+	
+	
+	"MenuWm": "Created By ${NamaOwner}",	
+	
+	
+	"ownerNumber": "6281216465289",
+	
+	
+	"NumberOwner": "+62 812-1646-5289",
+	
+	
+	"botNumber": "62812164652892",
+	
+	
+	"note": "HAI COK, LU SEHAT KAN?",
+	
+	
+	"VerifyBot": "MUKA LUH KEK AJG",
+	
+	
+	"LolhumanApi": "yusuf01ch1b917cApi",
+	
+	
+	"xteamApi": "FuzBot1",
+	
+	
+	"replyproses": "[🤙] PROSES DULU NGEMTOD",
+	
+	
+    "NaySend": "NIH ANJG, LU GA DONASI MATI ASU🗿",
+	
+	
+    "linkyt": "https://youtube.com/channel/UCZk_2e45Y2JEdg5mSkRishw",
+	
+	
+    "grub1": "https://chat.whatsapp.com/D5oT1vr2x6JE4H8E7fmunL",
+	
+	
+    "grub2": "https://chat.whatsapp.com/D5oT1vr2x6JE4H8E7fmunL",
+	
+	
+    "grub3": "https://chat.whatsapp.com/D5oT1vr2x6JE4H8E7fmunL"
+	
+	
+}
